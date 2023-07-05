d82fdc6e457757e470303484a23dfb59d9d9a3c6
Naman82 <b520031@iiit-bh.ac.in>
2023-07-05 11:11:13 +0530

    camera.py updated

--- Hand_Tracker/HandTrackingModule.py
+++ Hand_Tracker/HandTrackingModule.py
@@ -1,13 +1,11 @@
 import mediapipe as mp
-import cv2
-import time
 
 import numpy as np
 import pandas as pd
 
 
 class HandDetector():
-    def __init__(self,
+    def _init_(self,
                  maxHands = 2,
                  detectionCon = 0.5,
                  trackCon = 0.5):
@@ -26,56 +24,58 @@ class HandDetector():
         self.mpDraw = mp.solutions.drawing_utils
 
     def findHands(self, img):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        print(type(img))
+        img=np.asarray(img)
+        imgRGB = img[:, :, ::-1]
         results = self.hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                for id, lm in enumerate(handLms.landmark):
-                    h, w, c = img.shape
-                    cx, cy = int(lm.x * w), int(lm.y * h)
+        # if results.multi_hand_landmarks:
+        #     for handLms in results.multi_hand_landmarks:
+        #         for id, lm in enumerate(handLms.landmark):
+        #             h, w, c = img.shape
+        #             cx, cy = int(lm.x * w), int(lm.y * h)
                     # print(id, cx, cy)
 
-                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                # self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
 
         return results
 
-    def collect_landmarks(self, category_of_sample = None, save_file_name:str = "Landmarks.csv", num_samples:int = 1000):
-        cap = cv2.VideoCapture(0)
-        lm_cols = []
-        for lm_id in range(21):
-            for coord in ['x', 'y']:
-                lm_cols.append(f"{lm_id}_{coord}")
+    # def collect_landmarks(self, category_of_sample = None, save_file_name:str = "Landmarks.csv", num_samples:int = 1000):
+    #     cap = cv2.VideoCapture(0)
+    #     lm_cols = []
+    #     for lm_id in range(21):
+    #         for coord in ['x', 'y']:
+    #             lm_cols.append(f"{lm_id}_{coord}")
 
-        if category_of_sample:
-            lm_cols.append("Category")
+    #     if category_of_sample:
+    #         lm_cols.append("Category")
 
-        lms = pd.DataFrame(columns=lm_cols)
-        Stime = time.time()
-        pTime = 0
-        while len(lms) < num_samples:
-            success, img = cap.read()
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            results = self.hands.process(imgRGB)
+    #     lms = pd.DataFrame(columns=lm_cols)
+    #     Stime = time.time()
+    #     pTime = 0
+    #     while len(lms) < num_samples:
+    #         success, img = cap.read()
+    #         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #         results = self.hands.process(imgRGB)
 
-            if results.multi_hand_landmarks:
-                for handLms in results.multi_hand_landmarks:
-                    lm_dict = {}
-                    for id, Lm in enumerate(handLms.landmark):
-                        lm_dict[f"{id}_x"] = Lm.x
-                        lm_dict[f"{id}_y"] = Lm.y
-                    lm_dict['Category'] = category_of_sample
+    #         if results.multi_hand_landmarks:
+    #             for handLms in results.multi_hand_landmarks:
+    #                 lm_dict = {}
+    #                 for id, Lm in enumerate(handLms.landmark):
+    #                     lm_dict[f"{id}_x"] = Lm.x
+    #                     lm_dict[f"{id}_y"] = Lm.y
+    #                 lm_dict['Category'] = category_of_sample
 
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
-                    lms = lms.append(lm_dict, ignore_index=True)
-                    print(len(lms))
+    #                 self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+    #                 lms = lms.append(lm_dict, ignore_index=True)
+    #                 print(len(lms))
 
-            cv2.imshow("HandTrack", img)
-            pTime = time.time() - Stime
-            cv2.waitKey(1)
+    #         cv2.imshow("HandTrack", img)
+    #         pTime = time.time() - Stime
+    #         cv2.waitKey(1)
 
-        cv2.destroyAllWindows()
-        lms.to_csv(save_file_name)
+    #     cv2.destroyAllWindows()
+    #     lms.to_csv(save_file_name)
 
     def get_landmarks(self, img):
         # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -98,20 +98,20 @@ class HandDetector():
         else:
             return None
 
-    def get_results(self, img, classifier):
-        lms = self.get_landmarks(img)
-        result = "None"
-        if not (lms == "No Hand Tracked"):
+    # def get_results(self, img, classifier):
+    #     lms = self.get_landmarks(img)
+    #     result = "None"
+    #     if not (lms == "No Hand Tracked"):
 
-            pred = np.argmax((classifier.predict_proba(lms)), axis=-1)
-            if pred == 0:
-                result = "Paper"
-            elif pred == 1:
-                result = "Scissor"
-            elif pred == 2:
-                result = "Stone"
+    #         pred = np.argmax((classifier.predict_proba(lms)), axis=-1)
+    #         if pred == 0:
+    #             result = "Paper"
+    #         elif pred == 1:
+    #             result = "Scissor"
+    #         elif pred == 2:
+    #             result = "Stone"
 
-        return result
+    #     return result
 
         
 
@@ -130,8 +130,3 @@ class HandDetector():
                 elif(pred == 0):
                     predicted_as_dict['Paper'] = 1
         return predicted_as_dict
-
-
-
-
-
--- myapp/camera.py
+++ myapp/camera.py
@@ -1,6 +1,40 @@
-import cv2
+# import cv2
+# import sys
+# import joblib
+
+# BASE_DIR = "./"
+# sys.path.insert(0, (BASE_DIR + 'Hand_Tracker'))
+
+# from Hand_Tracker.HandTrackingModule import HandDetector
+# hand_detector = HandDetector()
+# classifier = joblib.load((BASE_DIR + "Hand_Tracker/Model/Predictor.pkl"))
+
+
+# class VideoCamera(object):
+#     def __init__(self):
+#         self.video=cv2.VideoCapture(0)
+
+#     def __del__(self):
+#         self.video.release()
+
+#     def get_frame(self):
+#         success,image=self.video.read()
+#         ret,jpeg=cv2.imencode('.jpg',image)
+#         return jpeg.tobytes()
+
+
+#     def get_result(self):
+#         success, image = self.video.read()
+#         predicted_data = hand_detector.get_result_as_dict(image, classifier)
+        
+#         return predicted_data
+
+import pygame
+from PIL import Image
 import sys
 import joblib
+import pygame.camera as camera
+from io import BytesIO
 
 BASE_DIR = "./"
 sys.path.insert(0, (BASE_DIR + 'Hand_Tracker'))
@@ -12,19 +46,37 @@ classifier = joblib.load((BASE_DIR + "Hand_Tracker/Model/Predictor.pkl"))
 
 class VideoCamera(object):
     def __init__(self):
-        self.video=cv2.VideoCapture(0)
+        pygame.init()
+        camera.init()
+        self.camlist = camera.list_cameras()
+        if not self.camlist:
+            raise ValueError("No cameras found.")
+        self.camera = camera.Camera(self.camlist[0], (640, 480))
+        self.camera.start()
 
     def __del__(self):
-        self.video.release()
+        self.camera.stop()
 
     def get_frame(self):
-        success,image=self.video.read()
-        ret,jpeg=cv2.imencode('.jpg',image)
-        return jpeg.tobytes()
+        frame = self.camera.get_image()
+        
+        # Convert the captured frame to a PIL Image
+        pil_image = Image.frombytes('RGB', frame.get_size(), pygame.image.tostring(frame, 'RGB'))
 
+        # Convert the PIL Image to JPEG bytes
+        with BytesIO() as output:
+            pil_image.save(output, format='JPEG')
+            jpeg_bytes = output.getvalue()
+
+        return jpeg_bytes
 
     def get_result(self):
-        success, image = self.video.read()
-        predicted_data = hand_detector.get_result_as_dict(image, classifier)
+        frame = self.camera.get_image()
         
+        # Convert the captured frame to a PIL Image
+        pil_image = Image.frombytes('RGB', frame.get_size(), pygame.image.tostring(frame, 'RGB'))
+
+        # Perform hand detection and classification
+        predicted_data = hand_detector.get_result_as_dict(pil_image, classifier)
+
         return predicted_data
